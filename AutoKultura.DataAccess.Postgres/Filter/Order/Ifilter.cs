c332c95421bb72d7b35410ff5659e8284720deff
Igor Tyushev <tyushev97@mail.ru>
2024-09-16 10:38:12 +1000

    Rename Folder DataAccess

--- AutoKultura.DataAccess.Postgres/Filter/Order/Ifilter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AutoKultura.DataAccess.SqlServer.Filter.Order
-{
-    public interface Ifilter<T>
-    {
-        IEnumerable<T> Filter(IEnumerable<T> items, Specification<T> spec);
-    }
-}
